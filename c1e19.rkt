e34e84aa660c1ad343646e4d548d8295ced1a392
Andrew Foltz-Morrison <andrew.foltz-morrison@sitomobile.com>
2017-04-28 17:53:09 -0400

    small progress on section 1, chapter 6.1

--- c1e19.rkt
+++ c1e19.rkt
@@ -61,3 +61,41 @@
                 (circle 10 "solid" "white"))))
 
 ; Exercise 94: the UFO approaches
+
+; Representations for the two possible game states
+(define-struct aim [ufo tank])
+(define-struct fired [ufo tank missile])
+
+; A UFO is a Posn.
+; interpretation (make-posn x y) is the UFO's location
+; (using the top-down, left-to-right convention)
+
+(define-struct tank [loc vel])
+; A Tank is a structure:
+;   (make-tank Number Number)
+; interpretation (make-tank x dx) specifies the position:
+; (x, HEIGHT) and the tank's speed: dx pixels/tick
+
+; A Missile is a Posn.
+; interpretation (make-posn x y) is the missile's place
+
+; A SIGS is one of:
+; - (make-aim UFO Tank)
+; - (make-fired UFO Tank Missile)
+; interpretation: represents the complete state of a
+; space invader game
+
+; Tank maneuvering into position to fire the missile
+(define aim1 (make-aim (make-posn 20 10) (make-tank 28 -3)))
+
+; The tank, UFO, and missile just after firing
+(define fired1 (make-fired (make-posn 20 10)
+                           (make-tank 28 -3)
+                           (make-posn (- HEIGHT TANK-HEIGHT))))
+
+; The tank, UFO, and missile just before the UFO is hit
+(define fired2 (make-fired (make-posn 20 100)
+                           (make-tank 100 3)
+                           (make-posn 22 103)))
+
+
